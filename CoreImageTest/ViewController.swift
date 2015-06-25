af9a790fa89888091f5e27420c054c4259b02230
Jia Jing <jiajing@client.openvpn.net>
2015-06-25 09:46:41 +0800

    Initial Commit

---
+++ CoreImageTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CoreImageTest
+//
+//  Created by Jia Jing on 6/25/15.
+//  Copyright © 2015 Jia Jing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
